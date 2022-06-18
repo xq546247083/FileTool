75a1ba12bdbd5377c6ff6b53b7a5247ca6028515
546247083@qq.com <546247083@qq.com>
2022-06-18 18:15:46 +0800

    初始化

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FileTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("文件工具");
+            Console.WriteLine("1、本程序会将【当前目录以及所有的子目录】的所有文件，移动到当前目录下");
+            Console.Write("输入对应的数字：");
+
+            var key = Console.ReadLine();
+
+            Console.WriteLine("正在操作，请稍候...");
+            if (key == "1")
+            {
+                var currentDir = System.Environment.CurrentDirectory;
+                FileManager.MoveAllFileToCurrentDir(currentDir, currentDir);
+            }
+
+            Console.WriteLine("操作完成，输入任意字符退出");
+            Console.Read();
+        }
+    }
+}
